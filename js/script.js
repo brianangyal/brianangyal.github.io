ca009e6d26b35a883beabe1d88848b562e0b851c
Brian Angyal <51534216+brianangyal@users.noreply.github.com>
2025-04-29 13:43:14 -0400

    yuhhhhhh

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 document.getElementById('enter-screen').addEventListener('click', () => {
     document.getElementById('enter-screen').style.display = 'none';
     document.getElementById('video-bg').style.display = 'block';
-    document.querySelector('.overlay').style.display = 'block';
     document.querySelector('.content').style.display = 'block';
 
     const video = document.getElementById('video-bg');
