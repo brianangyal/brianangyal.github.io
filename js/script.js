720118523994c3a23eb969759fcc49a5277b8842
Brian Angyal <51534216+brianangyal@users.noreply.github.com>
2025-04-29 14:22:54 -0400

    aaa

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 document.getElementById('enter-screen').addEventListener('click', () => {
+    const video = document.getElementById('video-bg');
+    const videoFiles = [
+        "heaven.mp4",
+        "blkkk.mp4"
+    ];
+    const selected = videoFiles[Math.floor(Math.random() * videoFiles.length)];
+    
+    video.muted = false;
+    video.querySelector("source").src = `videos/${selected}`;
+    video.load();
+
     document.getElementById('enter-screen').style.display = 'none';
-    document.getElementById('video-bg').style.display = 'block';
+    video.style.display = 'block';
     document.querySelector('.content').style.display = 'block';
 
-    const video = document.getElementById('video-bg');
-    video.muted = false;
-    video.play();
-  });
+    video.play().catch((error) => {
+        console.error("Playback failed:", error);
+    });
+});
